f71ba90c4baaecd3737d1a862cf8fd2cf501e7d1
Gnlow <gnlow@pm.me>
2025-06-01 14:15:33 +0900

    use button to onoff led

--- main.c
+++ main.c
@@ -1,31 +1,39 @@
 #include "gpio.c"
 
 int main(int argc, char* argv[]) {
-    int PIN = 20;
-    int POUT2 = 21;
+    int pout1 = 17;
+    int pout2 = 21;
+    int pin = 20;
+    
     int repeat = 100;
     int state = 1; 
     int prev_state = 1;
     int light = 0;
 
-    if (GPIOExport(POUT2) == -1 || GPIOExport(PIN)) {
-        return 1;
-    }
-    if (GPIODirection(POUT2, OUT) == -1 || GPIODirection(PIN, IN) == -1) {
-        return 2;
-    }
+    if (0
+        || GPIOExport(pout1) == -1
+        || GPIOExport(pout2) == -1
+        || GPIOExport(pin)
+    ) return 1;
+    if (0
+        || GPIODirection(pout1, OUT) == -1
+        || GPIODirection(pout2, OUT) == -1
+        || GPIODirection(pin, IN) == -1
+    ) return 2;
+
     do {
-        if (GPIOWrite(POUT2, 1) == -1) {
-            return 3;
-        }
-        printf("GPIORead: %d from pin %d\n", GPIORead(PIN), PIN);
+        if (GPIOWrite(pout2, 1) == -1) return 3;
+        printf("GPIORead: %d from pin %d\n", GPIORead(pin), pin);
+        if (GPIOWrite(pout1, GPIORead(pin)) == -1) return 3;
         usleep(1000 * 1000);
     } 
     while (repeat--);
     
-    if (GPIOUnexport(POUT2) == -1 || GPIOUnexport(PIN) == -1) {
-        return 4;
-    }
+    if (0
+        || GPIOUnexport(pout1) == -1
+        || GPIOUnexport(pout2) == -1
+        || GPIOUnexport(pin) == -1
+    ) return 4;
     
     return 0;
 }

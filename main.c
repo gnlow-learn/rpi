e2cf8452bd3f00b639d1ee1919f7275191a27603
Gnlow <gnlow@pm.me>
2025-06-01 14:27:01 +0900

    refactor

--- main.c
+++ main.c
@@ -1,5 +1,27 @@
 #include "gpio.c"
 
+#define export(p) do { \
+    if (GPIOExport(p) == -1) return 1; \
+} while (0)
+
+#define out(p) do { \
+    if (GPIODirection(p, OUT) == -1) return 1; \
+} while (0)
+
+#define in(p) do { \
+    if (GPIODirection(p, IN) == -1) return 2; \
+} while (0)
+
+#define write(p, x) do { \
+    if (GPIOWrite(p, x) == -1) return 3; \
+} while (0)
+
+#define read(p) GPIORead(p)
+
+#define unexport(p) do { \
+    if (GPIOUnexport(p) == -1) return 4; \
+} while (0)
+
 int main(int argc, char* argv[]) {
     int pout1 = 17;
     int pout2 = 21;
@@ -10,55 +32,25 @@ int main(int argc, char* argv[]) {
     int prev_state = 1;
     int light = 0;
 
-    if (0
-        || GPIOExport(pout1) == -1
-        || GPIOExport(pout2) == -1
-        || GPIOExport(pin)
-    ) return 1;
-    if (0
-        || GPIODirection(pout1, OUT) == -1
-        || GPIODirection(pout2, OUT) == -1
-        || GPIODirection(pin, IN) == -1
-    ) return 2;
+    export(pout1);
+    export(pout2);
+    export(pin);
+
+    out(pout1);
+    out(pout2);
+    in(pin);
 
     do {
-        if (GPIOWrite(pout2, 1) == -1) return 3;
-        printf("GPIORead: %d from pin %d\n", GPIORead(pin), pin);
-        if (GPIOWrite(pout1, GPIORead(pin)) == -1) return 3;
+        write(pout2, 1);
+        printf("GPIORead: %d from pin %d\n", read(pin), pin);
+        write(pout1, read(pin));
         usleep(1000 * 1000);
     } 
     while (repeat--);
-    
-    if (0
-        || GPIOUnexport(pout1) == -1
-        || GPIOUnexport(pout2) == -1
-        || GPIOUnexport(pin) == -1
-    ) return 4;
+
+    unexport(pout1);
+    unexport(pout2);
+    unexport(pin);
     
     return 0;
-}
-
-
-
-int main2(int argc, char* argv[]) {
-    int repeat = 10;
-    int pout = 17;
-    
-    if (GPIOExport(pout) == -1) {
-        return 1;
-    }
-    if (GPIODirection(pout, pout) == -1) {
-        return 2;
-    }
-    do {
-        if (GPIOWrite(pout, repeat % 2) == -1) {
-            return 3;
-        }
-        usleep(500 * 1000);
-    } while (repeat--);
-    if (GPIOUnexport(pout) == -1) {
-        return 4;
-    }
-
-  return 0;
 }

9b9f0eafc348bfca1baac44fb599014ffefb0bf6
Gnlow <gnlow@pm.me>
2025-06-01 14:06:00 +0900

    button

--- main.c
+++ main.c
@@ -1,6 +1,38 @@
 #include "gpio.c"
 
 int main(int argc, char* argv[]) {
+    int PIN = 20;
+    int POUT2 = 21;
+    int repeat = 100;
+    int state = 1; 
+    int prev_state = 1;
+    int light = 0;
+
+    if (GPIOExport(POUT2) == -1 || GPIOExport(PIN)) {
+        return 1;
+    }
+    if (GPIODirection(POUT2, OUT) == -1 || GPIODirection(PIN, IN) == -1) {
+        return 2;
+    }
+    do {
+        if (GPIOWrite(POUT2, 1) == -1) {
+            return 3;
+        }
+        printf("GPIORead: %d from pin %d\n", GPIORead(PIN), PIN);
+        usleep(1000 * 1000);
+    } 
+    while (repeat--);
+    
+    if (GPIOUnexport(POUT2) == -1 || GPIOUnexport(PIN) == -1) {
+        return 4;
+    }
+    
+    return 0;
+}
+
+
+
+int main2(int argc, char* argv[]) {
     int repeat = 10;
     int pout = 17;
     
